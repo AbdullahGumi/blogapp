34f47ee906c232153e2a0a913f47a1f1078afb00
Abdullah Gumi <abdulgumi77@gmail.com>
2021-01-28 10:50:07 +0100

    Implemented linting workflow

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
+const path = require('path');
 
 logger.info('connecting to', config.MONGODB_URI);
 
@@ -21,7 +22,10 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
   })
 
 app.use(cors());
-// app.use(express.static('build'))
+app.use(express.static(path.join(__dirname, '../client/build')))
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+// });
 app.use(express.json());
 app.use(middleware.requestLogger);
 app.use(middleware.tokenExtractor);
