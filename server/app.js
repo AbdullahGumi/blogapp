f36c0264a964544506a35cf98eebf993290f7b74
Abdullah Gumi <abdulgumi77@gmail.com>
2021-01-30 21:24:59 +0100

    refactored code to previous structure

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../client/build')))
 // app.get('/', function (req, res) {
-//   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+//   res.sendFile(path.join(__dirname, './client/build', 'index.html'));
 // });
 app.use(express.json());
 app.use(middleware.requestLogger);
